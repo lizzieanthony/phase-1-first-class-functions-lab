67c8cd6dffde1b3f55f878faa8e97d5a2908d9af
Lizzie Anthony <lizziewrab@me.com>
2021-11-02 20:50:01 -0500

    commit tree 4506c2dbbfe4ae30903dae9d1e5d878b56b2993d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(-2) 
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = x => {
+    return function(fare) {
+        return fare * x;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+
+
